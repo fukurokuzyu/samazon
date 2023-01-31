959aa477cd655c01a64dced823b2e2e8df3e2632
fukurokuzyu <aizawa.24@ideal-land.jp>
2023-01-31 12:15:42 +0000

    recommend_products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_31_022145) do
+ActiveRecord::Schema.define(version: 2023_01_31_112955) do
 
   create_table "admins", force: :cascade do |t|
     t.string "name", null: false
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2023_01_31_022145) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "recommended_flag", default: false
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
